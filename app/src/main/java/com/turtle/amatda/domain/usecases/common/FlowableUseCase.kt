4e175195d2e7bca4754cde348a494911d4ccf25d
sdk0213 <sdk0213@naver.com>
2021-06-12 15:29:43 +0900

    refactor : usecase 추상화 UseCase를 RxJava에 맞추어 추상화클래스 추가

---
+++ app/src/main/java/com/turtle/amatda/domain/usecases/common/FlowableUseCase.kt
@@ -0,0 +1,20 @@
+package com.turtle.amatda.domain.usecases.common
+
+import io.reactivex.Flowable
+import io.reactivex.Scheduler
+
+abstract class FlowableUseCase<Results, Params>
+    (
+    subscribeScheduler: Scheduler,
+    observeScheduler: Scheduler
+) : BaseReactiveUseCase(subscribeScheduler, observeScheduler){
+
+    abstract fun buildUseCaseCompletable(params: Params? = null): Flowable<Results>
+
+    fun execute(params: Params? = null) : Flowable<Results> {
+        return buildUseCaseCompletable(params)
+            .subscribeOn(subscribeScheduler)
+            .observeOn(observeScheduler)
+    }
+
+}

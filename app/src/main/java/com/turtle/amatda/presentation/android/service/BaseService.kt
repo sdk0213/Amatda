636a4aa917752237db1518a3944d9c0eac9eb9f3
sdk0213 <sdk0213@naver.com>
2021-10-24 20:24:31 +0900

    di : 서비스 컴포넌트 di 사용을 위해DaggerService 상속받음

--- app/src/main/java/com/turtle/amatda/presentation/android/service/BaseService.kt
+++ app/src/main/java/com/turtle/amatda/presentation/android/service/BaseService.kt
@@ -1,20 +1,11 @@
 package com.turtle.amatda.presentation.android.service
 
-import android.app.*
 import android.content.Intent
 import android.os.IBinder
-import android.util.Log
-import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
-import com.google.android.gms.location.Geofence
-import com.google.android.gms.location.GeofenceStatusCodes
-import com.google.android.gms.location.GeofencingEvent
-import com.turtle.amatda.R
-import com.turtle.amatda.presentation.utilities.foregroundChannelIdOfGeofence
-import com.turtle.amatda.presentation.utilities.notificationChannelIdOfGeofence
 import com.turtle.amatda.presentation.utilities.thisServiceIsForeGroundService
+import dagger.android.DaggerService
 
-abstract class BaseService : Service() {
+abstract class BaseService : DaggerService() {
 
     override fun onBind(p0: Intent?): IBinder? = null
 
@@ -22,7 +13,7 @@ abstract class BaseService : Service() {
         intent?.let {
 
             // 포어그라운드에서 알림창 없애기
-            if(it.getBooleanExtra(thisServiceIsForeGroundService, false)){
+            if (it.getBooleanExtra(thisServiceIsForeGroundService, false)) {
                 intent.putExtra(thisServiceIsForeGroundService, false)
                 startService(intent)
                 return START_NOT_STICKY

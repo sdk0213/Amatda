0ac13d419abd6dd20f13b3557d016d63f58b9746
sdk0213 <sdk0213@naver.com>
2021-12-04 15:07:52 +0900

    fix : 경험치 '0'일 때 표기 오류

--- app/src/main/java/com/turtle/amatda/domain/model/Level.kt
+++ app/src/main/java/com/turtle/amatda/domain/model/Level.kt
@@ -1,12 +1,12 @@
 package com.turtle.amatda.domain.model
 
 enum class Level(val rank: String, val level: Long, val exp: Long, val upExp: Long){
-    LEVEL_1("노란색 지도",1,100, 110),
-    LEVEL_2("초록색 지도",2,210, 120),
-    LEVEL_3("파란색 지도",3,330, 130),
-    LEVEL_4("빨강색 지도",4,460, 140),
-    LEVEL_5("주황색 지도",5,600, 150),
-    LEVEL_6("보라색 지도",6,750, 160),
-    LEVEL_7("검정색 지도",7,910, 170),
-    LEVEL_8("무지개색 지도",8,1080, 1000)
+    LEVEL_1("노란색 지도",1,0, 110),
+    LEVEL_2("초록색 지도",2,110, 120),
+    LEVEL_3("파란색 지도",3,230, 130),
+    LEVEL_4("빨강색 지도",4,360, 140),
+    LEVEL_5("주황색 지도",5,500, 150),
+    LEVEL_6("보라색 지도",6,650, 160),
+    LEVEL_7("검정색 지도",7,810, 170),
+    LEVEL_8("무지개색 지도",8,980, 1000)
 }
